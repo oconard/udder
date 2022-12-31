a56c6aa64c64c31f35890dc1f66771c4ef7ad5c8
olivier <oconard@shaw.ca>
2022-12-31 16:16:32 -0700

    Initial Commit

---
+++ udder/udderApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderApp.swift
+//  udder
+//
+//  Created by Olivier Conard on 2022-12-31.
+//
+
+import SwiftUI
+
+@main
+struct udderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
